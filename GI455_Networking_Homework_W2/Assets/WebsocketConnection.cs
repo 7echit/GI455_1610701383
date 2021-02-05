51e9c30d6a0943098886fbe108b4926b79b95b17
7echit <7echit@gmail.com>
2021-02-05 22:56:30 +0700

    Send Assignment Week2

---
+++ GI455_Networking_Homework_W2/Assets/WebsocketConnection.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using WebSocketSharp;
+
+namespace ProgramsChat
+{
+    public class WebsocketConnection : MonoBehaviour
+    {
+        public InputField IPTextField, PortTextField, ChatTextField;
+        
+        public Text TextField, RealTextField;
+        public Button SendMessageButton;
+        public GameObject ChatPanel;
+
+        WebSocket webSocket;
+
+        void Start()
+        {   
+            ChatPanel.SetActive(false);
+        }
+
+        public void OnConect()
+        {
+            webSocket = new WebSocket("ws://"+IPTextField.text+":"+PortTextField.text+"/");
+            webSocket.OnMessage += OnMessage;
+            
+            webSocket.Connect();
+            ChatPanel.SetActive(true);                     
+        }   
+
+        public void OnChat()
+        {
+            webSocket.Send(ChatTextField.text);
+   
+            RealTextField.text = TextField.text +'\n';
+        } 
+
+        private void OnDestroy() {
+            if(webSocket != null){
+                webSocket.Close();
+            }
+        }
+
+        public void OnMessage(object sender, MessageEventArgs e)
+        {   
+            if(e.IsText)
+            {
+                TextField.text = e.Data;
+                return;
+            }
+
+            Debug.Log("ข้อความจากเซิฟเวอร์ : " + e.Data);       
+        }
+    }
+}
+
+
